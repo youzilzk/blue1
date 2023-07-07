fc2e40f86453fb8bfc4646b6d3a9b1e9452ceef5
nyyxl <1290240016@qq.com>
2023-07-07 22:26:05 +0800

    心跳时间戳

--- app/src/main/java/com/youzi/blue/net/client/handlers/IdleCheckHandler.java
+++ app/src/main/java/com/youzi/blue/net/client/handlers/IdleCheckHandler.java
@@ -1,8 +1,15 @@
 package com.youzi.blue.net.client.handlers;
 
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import com.youzi.blue.net.common.protocol.Message;
 import com.youzi.blue.net.common.utils.LoggerFactory;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
@@ -22,12 +29,15 @@ public class IdleCheckHandler extends IdleStateHandler {
         super(READ_IDLE_TIME, WRITE_IDLE_TIME, 0);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void channelIdle(ChannelHandlerContext ctx, IdleStateEvent evt) {
         Channel channel = ctx.channel();
 
         if (IdleState.WRITER_IDLE == evt.state()) {
-            log.info("发送心跳[{}]", channel.id());
+            String heartId = String.valueOf(LocalDateTime.now().toEpochSecond(ZoneOffset.of("+8")));
+            log.info("发送心跳[channelId={},heartId={}]", channel.id(), heartId);
+            heartBeatMessage.setData(heartId.getBytes());
             ctx.channel().writeAndFlush(heartBeatMessage);
         }
     }
