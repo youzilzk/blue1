28adb8510e5c026efab8556ecd6955a558086264
youzi <1290240016@qq.com>
2023-07-07 14:58:53 +0800

    退出逻辑

--- app/src/main/java/com/youzi/blue/media/Encoder.kt
+++ app/src/main/java/com/youzi/blue/media/Encoder.kt
@@ -54,30 +54,32 @@ open class Encoder(
      */
     override fun run() {
         try {
-            var outputBufferIndex = codec.dequeueOutputBuffer(mBufferInfo, TIMEOUT_USEC.toLong())
-            while (!exit && outputBufferIndex >= 0) {
-                val outputBuffer = codec.getOutputBuffer(outputBufferIndex)!!
-                val outData = ByteArray(mBufferInfo.size)
-                outputBuffer[outData]
-                if (mBufferInfo.flags == MediaCodec.BUFFER_FLAG_CODEC_CONFIG) { // 含有编解码器初始化/特定的数据
-                    configbyte = outData
-                    Log.d(this.javaClass.name, "生成配置")
-
-                } else if (mBufferInfo.flags == MediaCodec.BUFFER_FLAG_KEY_FRAME) { // 关键帧
-                    Log.d(this.javaClass.name, "生成关键帧")
-                    val keyframe = ByteArray(mBufferInfo.size + configbyte.size)
-                    System.arraycopy(configbyte, 0, keyframe, 0, configbyte.size)
-                    System.arraycopy(outData, 0, keyframe, configbyte.size, outData.size)
-                    encoderListener!!.onH264(keyframe, 1, mBufferInfo.presentationTimeUs)
-                } else {
-                    //其他帧末
-                    encoderListener!!.onH264(outData, 2, mBufferInfo.presentationTimeUs)
-                }
-                codec.releaseOutputBuffer(outputBufferIndex, false)
-                outputBufferIndex =
+            while (!exit) {
+                var outputBufferIndex =
                     codec.dequeueOutputBuffer(mBufferInfo, TIMEOUT_USEC.toLong())
-            }
+                while (!exit && outputBufferIndex >= 0) {
+                    val outputBuffer = codec.getOutputBuffer(outputBufferIndex)!!
+                    val outData = ByteArray(mBufferInfo.size)
+                    outputBuffer[outData]
+                    if (mBufferInfo.flags == MediaCodec.BUFFER_FLAG_CODEC_CONFIG) { // 含有编解码器初始化/特定的数据
+                        configbyte = outData
+                        Log.d(this.javaClass.name, "生成配置")
 
+                    } else if (mBufferInfo.flags == MediaCodec.BUFFER_FLAG_KEY_FRAME) { // 关键帧
+                        Log.d(this.javaClass.name, "生成关键帧")
+                        val keyframe = ByteArray(mBufferInfo.size + configbyte.size)
+                        System.arraycopy(configbyte, 0, keyframe, 0, configbyte.size)
+                        System.arraycopy(outData, 0, keyframe, configbyte.size, outData.size)
+                        encoderListener!!.onH264(keyframe, 1, mBufferInfo.presentationTimeUs)
+                    } else {
+                        //其他帧末
+                        encoderListener!!.onH264(outData, 2, mBufferInfo.presentationTimeUs)
+                    }
+                    codec.releaseOutputBuffer(outputBufferIndex, false)
+                    outputBufferIndex =
+                        codec.dequeueOutputBuffer(mBufferInfo, TIMEOUT_USEC.toLong())
+                }
+            }
         } catch (e: Exception) {
             encoderListener?.onError(e)
             e.printStackTrace()
